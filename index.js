ac13c4dc17e53be5890ab8c166c796f845566eb6
ZDT@VAIO <dev@dthappy.com>
2016-06-06 23:39:07 +0800

    add a regex match

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 var http = require('http');
-http.get('http://www.google.com/index.html', (res) => {
-  console.log(`Got response: ${res.statusCode}`);
+var url = process.argv[2];
+var regx = process.argv[3];
+//import http from 'http';
+http.get(url, (res) => {
+  //console.log(`Got response: ${res.statusCode}`);
+  res.setEncoding('utf8');
+  res.on("data",(data) => {
+	//console.log(data);
+    data.match(regx).forEach((match) => {
+     console.log(match);
+    });
+    //console.log(data.match(/<img [^>]*\/>/gi));
+  });
   // consume response body
   res.resume();
-})
+}).on('error', (e) => {
+  console.log(`Got error: ${e.message}`);
+});;
