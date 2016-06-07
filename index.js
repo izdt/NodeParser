e726d9219248f75de51a332c6f653066587ca39b
izdt <dev@dthappy.com>
2016-06-07 22:03:57 +0800

    realize search from regex

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 var http = require('http');
 var url = process.argv[2];
-var regx = process.argv[3];
+var regexString = process.argv[3];
 //import http from 'http';
 http.get(url, (res) => {
   //console.log(`Got response: ${res.statusCode}`);
   res.setEncoding('utf8');
   res.on("data",(data) => {
 	//console.log(data);
-    data.match(regx).forEach((match) => {
+    //var regex = /\d+/g;
+    var regex = new RegExp(regexString,"g");
+    data.match(regex).forEach((match) => {
      console.log(match);
     });
     //console.log(data.match(/<img [^>]*\/>/gi));
@@ -16,4 +18,4 @@ http.get(url, (res) => {
   res.resume();
 }).on('error', (e) => {
   console.log(`Got error: ${e.message}`);
-});;
+});
