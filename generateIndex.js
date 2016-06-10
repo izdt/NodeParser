5558134add8eb732bda32673131e2e8545045616
izdt <dev@dthappy.com>
2016-06-10 15:48:22 +0800

    add a module to generate index.html with all imgs div

--- generateIndex.js
+++ generateIndex.js
@@ -1,9 +1,29 @@
 var fs = require('fs');
 
-var dir = "./";
+var dir = "../imgs/";
+var folders = [];
+
+
 var generateIndex = function(dir){
     var files = fs.readdirSync(dir);
-    console.log(files);
+    folders = files;
+    folders.forEach(function(element) {
+        if (fs.statSync(dir+element).isDirectory()){
+            var imgs = fs.readdirSync(dir+element);
+            var html = "";
+            imgs.forEach((item)=>{
+                var imgLine = `<img src="${item}" />`;
+                html+=imgLine;
+            });
+            fs.writeFile(dir+element+'/index.html', html, (err) => {
+                if (err) throw err;
+                console.log('It\'s saved!');
+            });
+            //console.log(html);
+        }
+        //console.log(dir+element);
+    }, this);
+    //console.log(files);
 };
 generateIndex(dir);
 
