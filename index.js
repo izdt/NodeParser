6abdca6f86a4de7110fbb63f2f9abeeab067fceb
ZDT@MAC <dev@dthappy.com>
2016-06-15 22:33:22 +0800

    change var to let

--- index.js
+++ index.js
@@ -4,38 +4,38 @@ const crawPage = require('./crawPage');
 
 
 const showData = function(matchData){
-    for(var i in matchData){
+    for(let i in matchData){
       console.log(matchData[i]);
     }
 };
 
 const saveImgs = function(matchData,url){
     //console.log(matchData);
-    var imgDir = decodeURI(url.substr(url.indexOf('.net/')+5)).replace('/','');
-    for(var i in matchData){
-      var imgUrl = matchData[i];
-      var imgName = imgUrl.substr(imgUrl.lastIndexOf('/')+1);
+    let imgDir = decodeURI(url.substr(url.indexOf('.net/')+5)).replace('/','');
+    for(let i in matchData){
+      let imgUrl = matchData[i];
+      let imgName = imgUrl.substr(imgUrl.lastIndexOf('/')+1);
       console.log("save img:" + imgName)
       imgSave(matchData[i], imgDir, imgName);
     }   
 };
 
 const crawImgPage = function(matchData) {
-    var imgRegex = "data-src=\"(http:\/\/crawling.*?)\"";
-    for(var i in matchData){
+    let imgRegex = "data-src=\"(http:\/\/crawling.*?)\"";
+    for(let i in matchData){
       crawPage(matchData[i],imgRegex,saveImgs);
     }
 };
 
 const crawList = function(url){
   //var url = process.argv[2];
-  var listRegex = "class=\"post-title entry-title\">\\s+<a href=\"(.*?)\"";
+  let listRegex = "class=\"post-title entry-title\">\\s+<a href=\"(.*?)\"";
   crawPage(url,listRegex,crawImgPage);
 };
 
 const batchCraw = function() {
-  var urls = process.argv[2].split(',');
-  var tUrls = [];
+  let urls = process.argv[2].split(',');
+  let tUrls = [];
   urls.forEach((u)=>{
     tUrls.push(encodeURI("http://"+u+"/"));
   });
